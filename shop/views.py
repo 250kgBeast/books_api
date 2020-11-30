538682c226e924ee459ee063fe62bae67b4d87d6
250kgBeast <59151069+250kgBeast@users.noreply.github.com>
2020-11-30 02:21:16 +0200

    feat: add Book model, serializer, view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from shop.models import Book
+from shop.serizalizers import BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
