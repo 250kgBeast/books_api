73249a2492c75ba740be8bbe24901ea7784fe74d
250kgBeast <59151069+250kgBeast@users.noreply.github.com>
2020-11-30 05:17:59 +0200

    test: test crud

--- shop/test/test_api.py
+++ shop/test/test_api.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
@@ -11,6 +14,7 @@ class BooksApiTestCase(APITestCase):
         self.book_2 = Book.objects.create(name='Book2', author_name='Author2', price='100.00')
         self.book_3 = Book.objects.create(name='Book3 Author2', author_name='Author3', price='300.00')
         self.url = reverse('book-list')
+        self.user = User.objects.create(username='test_username')
 
     def test_get(self):
         response = self.client.get(self.url)
@@ -61,3 +65,48 @@ class BooksApiTestCase(APITestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, serializer_data)
+
+    def test_permission_denied_for_unauthenticated_users(self):
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_create(self):
+        self.assertEqual(Book.objects.count(), 3)
+        data = {
+            "id": 1,
+            "name": "Programing in Python 3",
+            "author_name": "Mark Summerfield",
+            "price": "600.00"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(self.url, json_data,
+                                    content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Book.objects.count(), 4)
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        self.assertEqual(self.book_1.price, '100.00')
+        data = {
+            "name": self.book_1.name,
+            "author_name": self.book_1.author_name,
+            "price": 200
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, json_data,
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.book_1.refresh_from_db()
+        self.assertEqual(200, self.book_1.price)
+
+    def test_delete(self):
+        url = reverse('book-detail', args=(self.book_1.id, ))
+        self.assertTrue(Book.objects.filter(name='Book1').exists())
+        self.client.force_login(self.user)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Book.objects.filter(name='Book1').exists())
